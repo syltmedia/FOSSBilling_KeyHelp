e4a6076b93dc765a1cc9adbd6ac8f6bbb7817bc5
Syltmedia <122433487+syltmedia@users.noreply.github.com>
2023-01-15 12:23:39 +0100

    Create data.ts

---
+++ data.ts
@@ -0,0 +1,18 @@
+import { Extension } from '../interfaces'
+
+export const extensionData: Extension[] = [
+  {
+    id: 'ServerManagerKeyHelp',
+    type: 'lib',
+    name: 'KeyHelp',
+    description: 'Connecting KeyHelp control panel using the KeyHelp API v2',
+    author: {
+      name: 'Ole Sondermann',
+      URL: 'https://www.syltmedia.de'
+    },
+    download_url: 'https://github.com/syltmedia/FOSSBilling_KeyHelp/releases/download/0.0.2/ServerManagerKeyHelp.zip',
+    version: '0.0.2',
+    license: 'Open Source',
+    icon_url: ''
+  },
+]
